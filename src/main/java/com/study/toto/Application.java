a6fee3d21ca2a06a6a4e261ebf4b236b535cdaf3
aroma <dodo45133@gmail.com>
2018-04-22 21:17:29 +0900

    첫 번째 과제.

---
+++ src/main/java/com/study/toto/Application.java
@@ -0,0 +1,6 @@
+package com.study.toto;
+
+public class Application {
+    // TODO 2 Spring Application 시작점 만들기
+    //  SpringApplication.run(Application.class, args);
+}
---
+++ src/main/java/com/study/toto/controller/HelloRestController.java
@@ -0,0 +1,5 @@
+package com.study.toto.controller;
+
+public class HelloRestController {
+    // TODO 1 "/"로 요청이 들어오면 "Hello"을 리턴하는 RestController 작성
+}
